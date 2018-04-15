3ebde3e365dbda4ccfd9479e0e503544f165b97b
Krixium <bennywang75@hotmail.com>
2018-04-14 17:06:12 -0700

    Implemented for of the skeleton for the download manager

--- CommAudio/CommAudio/DownloadManager.cpp
+++ CommAudio/CommAudio/DownloadManager.cpp
@@ -4,11 +4,71 @@ DownloadManager::DownloadManager(QDir * source, QDir * downloads, QWidget * pare
 	: QWidget(parent)
 	, mSource(source)
 	, mDownloads(downloads)
+	, mServer(this)
 {
-
+	connect(&mServer, QTcpServer::newConnection, this, &DownloadManager::newConnectionHandler);
+	mServer.listen(QHostAddress::AnyIPv4, 42071);
 }
 
 void DownloadManager::DownloadFile(QString songName, quint32 address)
 {
-	
+	// Send download request here
+	// open qfile here for downloaded contents
+	// save it in map with address as key
+}
+
+void DownloadManager::newConnectionHandler()
+{
+	QTcpSocket * socket = mServer.nextPendingConnection();
+	quint32 address = socket->peerAddress().toIPv4Address();
+
+	mConnections[address] = socket;
+
+	connect(socket, &QTcpSocket::readyRead, this, &DownloadManager::incomingDataHandler);
+	connect(socket, &QTcpSocket::disconnected, this, &DownloadManager::disconnectHandler);
+}
+
+void DownloadManager::incomingDataHandler()
+{
+	QTcpSocket * socket = (QTcpSocket *)QObject::sender();
+	quint32 address = socket->peerAddress().toIPv4Address();
+	QByteArray data = socket->readAll();
+
+	switch (data[0])
+	{
+	case (char)Headers::RequestDownload:
+		uploadSong(data.mid(1), socket);
+		break;
+	case (char)Headers::RespondDownload:
+		writeToFile(data.mid(1), address);
+		break;
+	}
+}
+
+void DownloadManager::uploadSong(QByteArray data, QTcpSocket * socket)
+{
+	// Open the file
+
+	// in a while loop
+	// send chunks for the file
+
+	// close the file
+	// close the socket
+}
+
+void DownloadManager::writeToFile(QByteArray data, quint32 address)
+{
+	// write to the file that has the address as key	
+}
+
+void DownloadManager::disconnectHandler()
+{
+	QTcpSocket * socket = (QTcpSocket *)QObject::sender();
+	quint32 address = socket->peerAddress().toIPv4Address();
+
+	// remove socket from the map
+	mConnections.take(address)->deleteLater();
+
+	// Remove file from the map and close it
+	mFiles.take(address).close();
 }
--- CommAudio/CommAudio/DownloadManager.h
+++ CommAudio/CommAudio/DownloadManager.h
@@ -1,13 +1,17 @@
 #include <QDir>
 #include <QFile>
 #include <QHostAddress>
+#include <QMap>
 #include <QTcpServer>
 #include <QTcpSocket>
 #include <QWidget>
 
+#include "globals.h"
+
 class DownloadManager : public QWidget
 {
 	Q_OBJECT
+
 public:
 	DownloadManager(QDir * source, QDir * downloads, QWidget * parent = nullptr);
 	~DownloadManager() = default;
@@ -16,6 +20,20 @@ private:
 	QDir * mSource;
 	QDir * mDownloads;
 
+	QMap<quint32, QTcpSocket *> mConnections;
+	QMap<quint32, QFile> mFiles;
+
+	QTcpServer mServer;
+
+	void uploadSong(QByteArray data, QTcpSocket * socket);
+	void writeToFile(QByteArray data, quint32 address);
+
+private slots:
+	void newConnectionHandler();
+	void incomingDataHandler();
+	void disconnectHandler();
+
 public slots:
 	void DownloadFile(QString songName, quint32 address);
+
 };
